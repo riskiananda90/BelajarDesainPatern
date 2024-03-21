20a18f49a3800894ad61ecd95cd9c330e7f4ceb4
riski ananda <risskiananda90@gmail.com>
2024-03-21 11:33:34 +0700

    Ini_Contoh_Interface
    Ini_contoh_Inheritance

--- src/Duck.java
+++ src/Duck.java
@@ -1,92 +1,62 @@
-//Menggunakan Interface
-//### Tahap 2. Menggunakan Interface. (Rubah kode pada tahap 1 menjadi seperti berikut)
-//        1. Buat sebuah interface Quackable untuk perilaku bersuara.
-//        2. Buat sebuah interface Flyable untuk perilaku terbang.
-//        3. Implementasikan kedua interface tersebut dalam kelas Duck dan kelas-kelas turunannya.
-
-interface Quackable {
-    void quack();
-}
-
-interface Flyable {
-    void fly();
-}
-
 abstract class Duck {
-    abstract void display();
+    void Quack()
+    {
+        System.out.println(" Qwek Qwek");
+    }
     void Swim()
     {
         System.out.println("Berenang");
     }
-}
+    abstract void display();
 
-class Mallarduck extends Duck implements Quackable,Flyable{
-    @Override
-    public void quack() {
-        System.out.println("Quak Quak ");
-    }
-
-    @Override
-    public void fly() {
+    void fly()
+    {
         System.out.println("Terbang");
     }
+}
+
+class Mallarduck extends Duck{
     @Override
         void display()
         {
             System.out.println("Tampilan Mallarduck");
         }
 }
-class RedheadDuck extends Duck implements Quackable, Flyable {
-    @Override
-    public void quack() {
-        System.out.println("Qwek Qwek");
-    }
-
-    @Override
-    public void fly() {
-        System.out.println("Terbang");
-    }
-
-    @Override
-    void display() {
-        System.out.println("Tampilan RedHeadDuck");
-    }
-}
-class RubberDuck extends Duck implements Quackable{
-    @Override
-    public void quack() {
-        System.out.println("Squak");
-    }
-
+class RubberDuck extends Duck {
     @Override
     void display() {
         System.out.println("Tampilan RubberDuck");
     }
+
+    @Override
+    void Quack() {
+        System.out.println("Bunyinya Squeek bukan Qwak");
+    }
+
+    @Override
+    void fly() {
+//        Tidak ada implementasi karena bebek mainan tidak dapat terbang
+    }
 }
 
-class WoodenDuck extends Duck {
+class WoodenDuck extends Duck{
     @Override
     void display() {
         System.out.println("Tmapilan Woodenduck");
     }
 
-}
-
-class ToyDuck extends Duck implements Flyable {
     @Override
-    public void fly() {
-        System.out.println("Terbang seperti mainan");
+    void Quack() {
+//        tidak ada implementasi karena bebek mainan tidak dapat bersuara
     }
 
     @Override
-    void display() {
-        System.out.println("Tampilan ToyDuck");
+    void Swim() {
+        System.out.println("Mengambang");
+    }
+
+    @Override
+    void fly() {
+//        Tidak ada implementasi karena bebek mainan tidak dapat terbang
     }
 }
-//> _Analisis_:
-//> - Menggunakan interface untuk mendefinisikan perilaku bersuara dan terbang membuat kode lebih modular.
-//> - Menerapkan polimorfisme dengan menggunakan interface, yang memungkinkan berbagai implementasi perilaku bersuara dan terbang.
-//> - Kelas turunan dapat mengimplementasikan lebih dari satu perilaku bersuara atau terbang jika diperlukan.
-//> _Kekurangan_:
-//> - Masih terbatas dalam fleksibilitas saat ingin mengubah perilaku bersuara atau terbang secara dinamis pada saat runtime.
-//> - Jika ada perubahan pada perilaku bersuara atau terbang, perubahan tersebut masih perlu diterapkan pada semua kelas turunan yang terkait
